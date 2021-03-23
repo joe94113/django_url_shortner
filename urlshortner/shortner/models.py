ba88c889f027deacc3df1afae725e0ebb66648be
joe <joe94113@gmail.com>
2021-03-23 23:20:08 +0800

    commit create view

--- urlshortner/shortner/models.py
+++ urlshortner/shortner/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+# python manage.py makemigrations
+# 根據檢測到的模型創建新的遷移。遷移的作用，更多的是將數據庫的操作，以文件的形式記錄下來，方便以後檢查、調用、重做等等。
+# python manage.py migrate
+# 使數據庫狀態與當前模型集和遷移集同步。說白了，就是將對數據庫的更改，主要是數據表設計的更改，在數據庫中真實執行。
+# 例如，新建、修改、刪除數據表，新增、修改、刪除某數據表內的字段等等
+class Url(models.Model):
+    link = models.CharField(max_length=10000)
+    uuid = models.CharField(max_length=10)
--- urlshortner/shortner/views.py
+++ urlshortner/shortner/views.py
@@ -1,6 +1,19 @@
+import uuid  # 通用唯一辨識碼（Universally Unique Identifier，縮寫為 UUID）
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Url
 
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+
+def create(request):
+    if request.method == "POST":
+        link = request.POST["link"]
+        uid = str(uuid.uuid4())[:5]  # 產生第四版 UUID（隨機）
+        new_url = Url(link=link, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
